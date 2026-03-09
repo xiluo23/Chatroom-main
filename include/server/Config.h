80060f4eac2f37ffe2c35694bc35121a4c450524
xiluo23 <130372916+xiluo23@users.noreply.github.com>
2026-03-09 17:01:20 +0800

    Update Config.h

--- include/server/Config.h
+++ include/server/Config.h
@@ -68,7 +68,12 @@ public:
     }
 
 private:
-    Config() {}
+    Config() {
+        if (!loadConfig("/root/Chatroom-main/src/server/server.conf")) {
+            std::cerr << "Failed to load config file " << std::endl;
+            // 也可以选择退出，或者使用默认值继续
+        }
+    }
     ~Config() {}
 
     std::unordered_map<std::string, std::string> _configMap;
